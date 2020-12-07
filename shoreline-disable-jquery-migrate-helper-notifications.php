4d7be646287658031190b568938877f384f173da
shoreline-chrism <chrism@shorelinemedianj.com>
2020-12-07 15:02:36 -0500

    Fixing description and version

--- shoreline-disable-jquery-migrate-helper-notifications.php
+++ shoreline-disable-jquery-migrate-helper-notifications.php
@@ -1,11 +1,11 @@
 <?php 
 /**
  * Plugin Name: Disable "Enable jQuery Migrate Helper" plugin email notifications
- * Plugin Author: Shoreline Media
- * Plugin Homepage: https://shoreline.media
- * Plugin Description: The "Enable jQuery Migrate Helper" plugin enables support for older and outdated plugins during Wordpress' update of jQuery, but it also emails the <code>admin_email</code> for the site every week. This plugin disables email notifications entirely, even for automatic downgrades.
+ * Author: Shoreline Media
+ * Plugin URI: https://shoreline.media
+ * Description: The "Enable jQuery Migrate Helper" plugin enables support for older and outdated plugins during Wordpress' update of jQuery, but it also emails the <code>admin_email</code> for the site every week. This plugin disables email notifications entirely, even for automatic downgrades.
  * GitHub Plugin URI: https://github.com/shorelinemedia/shoreline-disable-jquery-migrate-helper-notifications
- * Plugin Version: 1.0.1
+ * Version: 1.0.1
  */
 
 if ( !function_exists( 'sl9_jqmh_email_message_disable' ) ) {
